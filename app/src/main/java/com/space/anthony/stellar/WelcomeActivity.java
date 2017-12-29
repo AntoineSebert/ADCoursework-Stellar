22c803bda8ec5b10fad19e3c753e49d7628af4a8
Antoine <antoine.sb@orange.fr>
2017-12-29 13:47:09 +0100

    Création de l'écran d'accueil

--- app/src/main/java/com/space/anthony/stellar/WelcomeActivity.java
+++ app/src/main/java/com/space/anthony/stellar/WelcomeActivity.java
@@ -9,5 +9,10 @@ public class WelcomeActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_welcome);
+        try {
+            Thread.sleep(3000);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
     }
 }
