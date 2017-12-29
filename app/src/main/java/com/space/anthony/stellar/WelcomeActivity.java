035fcaceb9b527a9a1a04a8c446cc3ce96b3e929
Antoine <antoine.sb@orange.fr>
2017-12-29 13:58:49 +0100

    Création de LoginActivity, ajout du passage de WelcomeActivity à LoginActivity

--- app/src/main/java/com/space/anthony/stellar/WelcomeActivity.java
+++ app/src/main/java/com/space/anthony/stellar/WelcomeActivity.java
@@ -1,5 +1,7 @@
 package com.space.anthony.stellar;
 
+import android.content.Intent;
+import android.os.Handler;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
@@ -9,10 +11,17 @@ public class WelcomeActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_welcome);
-        try {
-            Thread.sleep(3000);
-        } catch (InterruptedException e) {
-            e.printStackTrace();
-        }
+        Handler handler = new Handler();
+        handler.postDelayed(new Runnable() {
+            public void run() {
+                try {
+                    Thread.sleep(5000);
+                    Intent intent = new Intent(WelcomeActivity.this, LoginActivity.class);
+                    startActivity(intent);
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+            }
+        }, 3000);
     }
 }
