550f866c38d8e575dff3383f35990829d29266e1
Antoine <antoine.sb@orange.fr>
2018-01-01 18:16:26 +0100

    Ajout de l'activité du menu principal

--- app/src/main/java/com/space/anthony/stellar/LoginActivity.java
+++ app/src/main/java/com/space/anthony/stellar/LoginActivity.java
@@ -3,6 +3,7 @@ package com.space.anthony.stellar;
 import android.animation.Animator;
 import android.animation.AnimatorListenerAdapter;
 import android.annotation.TargetApi;
+import android.content.Intent;
 import android.content.pm.PackageManager;
 import android.support.annotation.NonNull;
 import android.support.design.widget.Snackbar;
@@ -87,6 +88,13 @@ public class LoginActivity extends AppCompatActivity implements LoaderCallbacks<
 					return true;
 				}
 				return false;
+			}
+		});
+
+		findViewById(R.id.email_sign_in_up_button).setOnClickListener(new View.OnClickListener() {
+			@Override
+			public void onClick(View v) {
+				attemptLogin();
 			}
 		});
 
@@ -369,6 +377,8 @@ public class LoginActivity extends AppCompatActivity implements LoaderCallbacks<
 
 			if (success) {
 				finish();
+				Intent intent = new Intent(LoginActivity.this, MainMenuActivity.class);
+				startActivity(intent);
 			} else {
 				mPasswordView.setError(getString(R.string.error_incorrect_password));
 				mPasswordView.requestFocus();
