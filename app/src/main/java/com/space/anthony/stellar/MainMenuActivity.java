392e34f9978c52a745cab06f08a891453d54a706
Antoine <antoine.sb@orange.fr>
2018-01-02 16:48:06 +0100

    Gestion de l'activation du bouton "Resume", implémentation partielle du bouton de sortie de l'app, implémentation partielle du bouton "New Game"

--- app/src/main/java/com/space/anthony/stellar/MainMenuActivity.java
+++ app/src/main/java/com/space/anthony/stellar/MainMenuActivity.java
@@ -1,7 +1,18 @@
 package com.space.anthony.stellar;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ToggleButton;
+
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
 
 public class MainMenuActivity extends AppCompatActivity {
 
@@ -9,5 +20,59 @@ public class MainMenuActivity extends AppCompatActivity {
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_main_menu);
+		DatabaseReference myRef = FirebaseDatabase.getInstance().getReference();
+
+		// RESUME //////////////////////////////////////////////////////////////////////////////////
+		myRef.addListenerForSingleValueEvent(new ValueEventListener() {
+			@Override
+			public void onDataChange(DataSnapshot dataSnapshot) {
+				String value = dataSnapshot.getValue(String.class);
+				if (value != null) {
+					ToggleButton resumeButton = findViewById(R.id.resume_button);
+					resumeButton.setEnabled(true);
+				}
+			}
+			@Override
+			public void onCancelled(DatabaseError error) {
+				Log.w("DATABASE", "Failed to read value.", error.toException());
+			}
+		});
+
+		// NEW GAME ////////////////////////////////////////////////////////////////////////////////
+		Button newGameButton = findViewById(R.id.new_game_button);
+		newGameButton.setOnClickListener(new View.OnClickListener() {
+			@Override
+			public void onClick(View v) {
+				FirebaseDatabase.getInstance().getReference().addListenerForSingleValueEvent(new ValueEventListener() {
+					@Override
+					public void onDataChange(DataSnapshot dataSnapshot) {
+						String value = dataSnapshot.getValue(String.class);
+						if (value != null) {
+							finish();
+							Intent intent = new Intent(MainMenuActivity.this, MainMenuActivity.class);
+							startActivity(intent);
+						}
+						else {
+
+						}
+					}
+					@Override
+					public void onCancelled(DatabaseError error) {
+						Log.w("DATABASE", "Failed to read value.", error.toException());
+					}
+				});
+			}
+		});
+
+
+		// QUIT ////////////////////////////////////////////////////////////////////////////////////
+		Button quitButton = findViewById(R.id.quit_button);
+		quitButton.setOnClickListener(new View.OnClickListener() {
+			@Override
+			public void onClick(View v) {
+				finish();
+				System.exit(0);
+			}
+		});
 	}
 }
--- app/src/main/java/com/space/anthony/stellar/WelcomeActivity.java
+++ app/src/main/java/com/space/anthony/stellar/WelcomeActivity.java
@@ -7,16 +7,16 @@ import android.os.Bundle;
 
 public class WelcomeActivity extends AppCompatActivity {
 
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_welcome);
-        Handler handler = new Handler();
-        handler.postDelayed(new Runnable() {
-            public void run() {
-                Intent intent = new Intent(WelcomeActivity.this, LoginActivity.class);
-                startActivity(intent);
-            }
-        }, 4000);
-    }
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_welcome);
+		Handler handler = new Handler();
+		handler.postDelayed(new Runnable() {
+			public void run() {
+				Intent intent = new Intent(WelcomeActivity.this, LoginActivity.class);
+				startActivity(intent);
+			}
+		}, 4000);
+	}
 }
